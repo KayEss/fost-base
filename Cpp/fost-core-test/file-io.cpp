ebe6ec447df5202e99146a687761cba39829775c
Kirit Sælensminde <kirit@felspar.com>
2025-05-24 11:40:42 +0700

    Fix IO test so it runs on newer CMake versions

--- Cpp/fost-core-test/file-io.cpp
+++ Cpp/fost-core-test/file-io.cpp
@@ -10,7 +10,7 @@ FSL_MAIN(
         "fost-core-test-file-io",
         "fost-core-test-file-io\n"
         "Test file I/O and its Unicode handling\n"
-        "Copyright (C) 2009-2020 Red Anchor Trading Co. Ltd.")
+        "Copyright (C) 2009-2025 Kirit Saelensminde")
 (fostlib::ostream &out, fostlib::arguments &args) {
     auto filename(fostlib::coerce<std::filesystem::path>(args[1].value()));
     // Check that we can do some basic reads
@@ -22,7 +22,7 @@ FSL_MAIN(
         outfile.write("\n", 1);
     }
     felspar::u8string loaded = fostlib::utf::load_file(filename);
-    if (loaded != felspar::u8string{u"abcdef\n\x2122\n"}) {
+    if (loaded != felspar::u8string{u"abcdef\n\u2122\n"}) {
         out << "File loaded did not match file saved\n"
                "97 98 99 100 101 102 10 8482 10"
             << std::endl;
